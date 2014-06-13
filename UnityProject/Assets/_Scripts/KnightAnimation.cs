71bf73f022aa3c8e8f50d320ace38c887cdc989e
NeoLeijona <danteg18@hotmail.com>
2014-06-13 14:11:35 +0300

    Start of KnightAnimation

---
+++ UnityProject/Assets/_Scripts/KnightAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnightAnimation : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			anim.SetBool("RightSwing",true);
+		}
+	
+	}
+}
