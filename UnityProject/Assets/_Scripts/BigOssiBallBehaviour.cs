a6c4f63553d15944aad4eb12851cb5816c432400
Asko <metabug@gmail.com>
2014-06-18 12:40:32 +0300

    Continued bigOssi behaviour

--- UnityProject/Assets/_Scripts/BigOssiBallBehaviour.cs
+++ UnityProject/Assets/_Scripts/BigOssiBallBehaviour.cs
@@ -4,41 +4,34 @@ using System.Collections;
 public class BigOssiBallBehaviour : MonoBehaviour {
 
 	public BigOssiBehaviour bigOssi;
-
-	protected bool isInPosition = false;
+	public float shootTimeLimit = 0;
+	
 	protected Vector3 startPos;
-	protected Vector3 endPos;
 	protected float spawnTime;
+	
 	// Use this for initialization
 	void Start () {
 		bigOssi = GameObject.Find ("BigOssi").GetComponent<BigOssiBehaviour> ();
 		startPos = bigOssi.transform.position;
 		gameObject.transform.position = new Vector3(startPos.x - 1.4f,startPos.y);
-		endPos = new Vector3 (gameObject.transform.position.x, 1.0f);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		spawnTime += Time.deltaTime;
-//
-//		if (gameObject.transform.position.y > endPos.y)
-//		{
-//			gameObject.transform.position = Vector3.Lerp(startPos, endPos, spawnTime);
-//		} 
-//		else
-//		{
-//			isInPosition = true;
-//		}
-		//if (isInPosition == true)
+		gameObject.transform.Translate(new Vector3((Mathf.Sin(spawnTime*2.24f)/20.0f),Mathf.Cos(spawnTime*2.24f)/20.0f));
+
+	}
+
+	protected virtual void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag ("Deflected"))
 		{
-			gameObject.transform.Translate(new Vector3((Mathf.Sin(spawnTime*2.24f)/20.0f),
-			                                           Mathf.Cos(spawnTime*2.24f)/20.0f));
-
-
-			//transform.Translate(Time.deltaTime*5.5f,Time.deltaTime*5.5f,0); // move forward
-			//transform.Rotate(0,0,Time.deltaTime*10.5f); // turn a little
+			Destroy(collision.gameObject);
+			bigOssi.Delete(gameObject);
+			Destroy(gameObject);
 		}
-
 	}
 }
--- UnityProject/Assets/_Scripts/BigOssiBehaviour.cs
+++ UnityProject/Assets/_Scripts/BigOssiBehaviour.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 
 public class BigOssiBehaviour : MonoBehaviour {
 
+	public GameObject projectilePrefab;
 	public GameObject shieldBallPrefab;
-	public float ballCount = 10f;
-	
+	public float ballCount = 5f;
+	public float shootingSpeed = 0f;
+
 	protected List<GameObject> shieldBalls = new List<GameObject>();
 	protected bool isSpawningBalls = true;
 	protected float spawnTime = 0f;
+	
+	protected float _shootTimer = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,15 +21,15 @@ public class BigOssiBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(isSpawningBalls == true)
-		SpawnBalls ();
-
-
+		if (isSpawningBalls == true)
+			SpawnBalls ();
+		else {
+			ShootBalls();
+		}
 	}
 
 	public void Spawn()
 	{
-		
 		shieldBalls.Add((GameObject)Instantiate(shieldBallPrefab,
 		                        gameObject.transform.position,
 		                        gameObject.transform.rotation));
@@ -35,12 +39,30 @@ public class BigOssiBehaviour : MonoBehaviour {
 	public void SpawnBalls()
 	{
 		spawnTime += Time.deltaTime;
-		if (spawnTime >= 0.2f)
+		if (spawnTime >= 0.4f)
 		{
 			Spawn();
 			spawnTime = 0f;
 		}
+
 		if (shieldBalls.Count >= ballCount)
 			isSpawningBalls = false;
+	}
+
+	public void ShootBalls()
+	{
+		_shootTimer += Time.deltaTime;
+		if (_shootTimer >= 5.0f)
+		{
+			_shootTimer = 0.0f;
+			GameObject tempo = (GameObject)Instantiate (projectilePrefab, transform.position, transform.rotation);
+			tempo.GetComponent<BallBehaviour>().SetStartVelocity(new Vector2(Random.Range(-0.2f, 0.2f), -0.4f));
+			//ListDeflectable(tempo);
+		}
+	}
+
+	public void Delete(GameObject me)
+	{
+		Destroy (me);
 	}
 }
