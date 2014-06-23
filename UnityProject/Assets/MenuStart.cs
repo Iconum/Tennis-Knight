d81f25eaa54d28825814aded3ca27a9fcb19e5c8
NeoLeijona <danteg18@hotmail.com>
2014-06-23 15:01:05 +0300

    Started menu NOTE: build settings changed, if nothing works: File->Build Settings... change what you need

---
+++ UnityProject/Assets/MenuStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+//		{
+//			Application.LoadLevel("testi2");
+//		}
+	}
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel("testi2");
+	}
+}
