459df5ce798cf57957923e9070c00d4f5ff29868
Hertzila <kat12ssamulih>
2014-06-27 15:02:52 +0300

    Castle Raid view work continued

--- UnityProject/Assets/_Scripts/CastleRaidHandler.cs
+++ UnityProject/Assets/_Scripts/CastleRaidHandler.cs
@@ -5,24 +5,46 @@ using System.Collections.Generic;
 public class CastleRaidHandler : MonoBehaviour {
 	public LevelBehaviour level = null;
 	public GameObject terrainPrefab, castlePrefab, tinyVillagerPrefab;
-	public List<GameObject> moneyPrefabs;
-	public Vector3 startLocation, endLocation;
+	public Vector3 startLocation, endLocation, castleOffset;
+	public int villagerBundleAmount = 20;
 
 	protected bool _displaying = false;
 	protected GameObject _terrain, _castle;
 	protected List<GameObject> _villagers;
-
-	// Update is called once per frame
-	void Update () {
-		if (_displaying)
-		{
-
-		}	
-	}
+	protected int _spawnedBundles = 0;
 
 	public void Display()
 	{
 		_displaying = true;
+		_terrain = (GameObject)Instantiate (terrainPrefab, transform.position, transform.rotation);
+		_castle = (GameObject)Instantiate (castlePrefab, endLocation + castleOffset, Quaternion.Euler (Vector3.zero));
+		SpawnBundle ();
+	}
 
+	public void SpawnBundle()
+	{
+		if (villagerBundleAmount * _spawnedBundles > Statics.villagers)
+		{
+			++_spawnedBundles;
+			GameObject tempo = (GameObject)Instantiate (tinyVillagerPrefab, startLocation, Quaternion.Euler (Vector3.zero));
+			tempo.GetComponent<BundleBehaviour> ().GetInitialValues (this, _castle, endLocation, startLocation);
+			_villagers.Add (tempo);
+			StartCoroutine (SpawnDelay ());
+		}
+	}
+	IEnumerator SpawnDelay()
+	{
+		yield return new WaitForSeconds (0.5f);
+		SpawnBundle ();
+	}
+
+	public void RemoveFromList(GameObject me)
+	{
+		_villagers.Remove (me);
+	}
+
+	public int GetVillagerCount()
+	{
+		return _villagers.Count;
 	}
 }
