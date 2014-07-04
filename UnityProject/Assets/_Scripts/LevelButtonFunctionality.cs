6b224223676341bdd3d9fadfb7fd49767600cbd6
Hertzila <kat12ssamulih>
2014-07-04 10:48:44 +0300

    Level button functionality

---
+++ UnityProject/Assets/_Scripts/LevelButtonFunctionality.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelButtonFunctionality : ButtonFunctionality {
+	public ButtonTypes buttonType;
+	public enum ButtonTypes
+	{
+		Back,
+		Shop,
+		Tutorial,
+		First,
+		Second,
+		Third
+	}
+
+	protected override void PressButton ()
+	{
+		base.PressButton ();
+		switch (buttonType)
+		{
+		case ButtonTypes.Back:
+				Application.LoadLevel("Menu");
+			break;
+		case ButtonTypes.Shop:
+
+			break;
+		case ButtonTypes.Tutorial:
+			break;
+		case ButtonTypes.First:
+			break;
+		case ButtonTypes.Second:
+			break;
+		case ButtonTypes.Third:
+			break;
+		}
+	}
+}
