934e27004858cfb36845a1ace8b2d8e5bad26981
Asko <metabug@gmail.com>
2014-07-08 13:05:09 +0300

    Level selection movement now works

--- UnityProject/Assets/_Scripts/LevelSelFunctionality.cs
+++ UnityProject/Assets/_Scripts/LevelSelFunctionality.cs
@@ -7,13 +7,14 @@ public class LevelSelFunctionality : MonoBehaviour {
 	public int levelsCleared = 0;
 	public int lastClearedLevel = 0;
 	public GameObject levelSelectorPrefab;
-	public GameObject wayPointPrefab;
+	public GameObject wayPointHandlerPrefab;
 
 	protected int _curPoint = 0,_nextPoint = 0,_tempPoint = 0,_waypointCount = 0;
 	protected Vector3 _curPointPos;
 	protected Vector3 _nextPointPos;
 	protected Vector3 _tempPointPos;
-	protected List<Vector3> _wayPoints = new List<Vector3>();
+	protected List<GameObject> _levels = new List<GameObject>();
+	protected GameObject _level;
 	protected GameObject levelSelector;
 	protected bool isInPosition = true;
 
@@ -22,47 +23,61 @@ public class LevelSelFunctionality : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		levelsCleared = wayPointPrefab.gameObject.GetComponent<PointLevel>().levelID;
+		for (int i = 0; wayPointHandlerPrefab.transform.childCount > i; ++i)
+		{
+			_level = wayPointHandlerPrefab.transform.GetChild(i).gameObject;
+			_levels.Add(_level);
+		}
 		levelSelector = (GameObject)Instantiate ( levelSelectorPrefab,
-		                                         _curPointPos,
+		                                         _levels[levelsCleared].transform.position,
 	                                          	  levelSelectorPrefab.transform.rotation);
+		setPoint (0);
+
 	}
 	// Update is called once per frame
 	void Update () 
 	{
 		levelSelector.transform.position = new Vector3 (_curPointPos.x, 
-		                                                _curPointPos.y + Mathf.Cos(Time.time*4)/4);
-
+		                                                _curPointPos.y + Mathf.Cos(Time.time*2)/6);
 		if (isInPosition == false)
 		{
-			move ();
+			move (_curPointPos,_levels[_nextPoint].transform.position);
 		}
 
 		//Optional
 		keyboardThings ();
 	}
 
-	protected void move ()
+	protected void move (Vector3 from, Vector3 to)
 	{
-		_curPointPos = Vector3.Lerp (_curPointPos, _nextPointPos, Time.deltaTime);
+		_curPointPos = Vector3.Lerp (from, to, Time.deltaTime*4);
+		if (_curPointPos == to)
+			isInPosition = true;
 	}
 
 	public void setPoint(int pointID)
 	{
 		_nextPoint = pointID;
+		isInPosition = false;
 	}
-
+	
 	protected void keyboardThings()
 	{
 		if (Input.GetKeyDown (KeyCode.Alpha1))
 		{
 			setPoint(0);
-			isInPosition = false;
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha2))
 		{
 			setPoint(1);
-			isInPosition = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3))
+		{
+			setPoint(2);
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4))
+		{
+			setPoint(3);
 		}
 	}
 }
