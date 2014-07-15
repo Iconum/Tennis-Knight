e1765df4c4aa588b6a155c324622ba1c93bb0f3e
Asko <metabug@gmail.com>
2014-07-15 15:21:10 +0300

    started dragon baby boss

---
+++ UnityProject/Assets/DragonBabyBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonBabyBehaviour : EnemyBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		base.Update ();
+	
+	}
+
+	protected void bossPhaseSetter()
+	{
+		switch (health)
+		{
+		case 5:
+
+			break;
+		case 4:
+
+			break;
+		case 3:
+
+			break;
+		case 2:
+
+			break;
+		case 1:
+
+			break;
+		default:
+
+			break;
+		}
+	}
+
+}
