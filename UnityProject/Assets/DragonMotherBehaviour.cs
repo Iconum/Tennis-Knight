90f19885095897e5184328ffad7f037b8ae66686
Asko <metabug@gmail.com>
2014-07-21 13:15:29 +0300

    updated boss

--- UnityProject/Assets/DragonMotherBehaviour.cs
+++ UnityProject/Assets/DragonMotherBehaviour.cs
@@ -12,7 +12,13 @@ public class DragonMotherBehaviour : EnemyBehaviour {
 	
 	public GameObject[] Projectiles = null;
 	protected GameObject curProjectile;
-	
+
+	public GameObject explosionPrefab = null;
+
+	protected bool isDead = false;
+	protected float deathTimer = 0f;
+	protected float explosiontimer = 0f;
+
 	public float shootingSpeed = 0f;
 	protected float _shootTimer = 0f;
 	protected List<GameObject> ownProjectiles = new List<GameObject> ();
@@ -23,9 +29,63 @@ public class DragonMotherBehaviour : EnemyBehaviour {
 		shootTimer += Time.deltaTime;
 
 		base.Update ();
-		if(!spawning)
+		if(!spawning && !isDead)
 		ShootBalls ();
+		if (isDead)
+			deathAnim ();
 		
+	}
+
+	protected override void DamageHealth()
+	{
+		if (!_flickerActive && !isDead)
+		{
+			--health;
+			_flickerActive = true;
+			if (anim)
+				anim.SetTrigger ("Damage");
+			
+			if (health <= 0)
+			{
+				isDead = true;
+				//Destroy (gameObject);
+			}
+		}
+	}
+
+	protected override void Flicker ()
+	{
+		_flickerTimer += Time.deltaTime;
+		if (_flickerTimer % 0.1f < 0.05f)
+		{
+			GetComponent<SpriteRenderer>().color = new Color(0.7f,0.7f,1f);
+		} else
+		{
+			GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f);
+		}
+		if (_flickerTimer > flickerTimerLimit)
+		{
+			_flickerActive = false;
+			GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f);
+			_flickerTimer = 0.0f;
+		}
+	}
+
+	protected void deathAnim()
+	{
+		deathTimer += Time.deltaTime;
+		explosiontimer += Time.deltaTime;
+		transform.position = new Vector2 (transform.position.x + Random.Range (0, 1), transform.position.y + Random.Range (0, 1));
+		if (explosiontimer >= 0.15f)
+		{
+			var explosion = Instantiate(explosionPrefab,
+			                            new Vector2(transform.position.x + Random.Range (-3, 3), transform.position.y + Random.Range (-3, 3)),
+			                            transform.rotation);
+			explosiontimer = 0;
+		}
+		if (deathTimer >= 5f)
+			Destroy (gameObject);
+
 	}
 	
 	public void ShootBalls()
---
+++ UnityProject/Assets/ExplosionAnimBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExplosionAnimBehaviour : MonoBehaviour {
+
+	protected Animator anim;
+	protected float timer = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+		if (timer >= 0.3f)
+			Destroy (gameObject);
+	}
+}
